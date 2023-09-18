2a36e6e0bb9bb3395352893fda82a653a82529ee
martmull <martmull@hotmail.com>
2023-09-18 13:36:29 +0200

    Remove everything from ee/ folder in confirmation handler

--- packages/features/bookings/lib/handleConfirmation.ts
+++ packages/features/bookings/lib/handleConfirmation.ts
@@ -3,8 +3,6 @@ import type { Prisma, Workflow, WorkflowsOnEventTypes, WorkflowStep } from "@pri
 import type { EventManagerUser } from "@calcom/core/EventManager";
 import EventManager from "@calcom/core/EventManager";
 import { sendScheduledEmails } from "@calcom/emails";
-import { isEventTypeOwnerKYCVerified } from "@calcom/features/ee/workflows/lib/isEventTypeOwnerKYCVerified";
-import { scheduleWorkflowReminders } from "@calcom/features/ee/workflows/lib/reminders/reminderScheduler";
 import getWebhooks from "@calcom/features/webhooks/lib/getWebhooks";
 import { scheduleTrigger } from "@calcom/features/webhooks/lib/scheduleTrigger";
 import type { EventTypeInfo } from "@calcom/features/webhooks/lib/sendPayload";
@@ -13,7 +11,6 @@ import { getTeamIdFromEventType } from "@calcom/lib/getTeamIdFromEventType";
 import logger from "@calcom/lib/logger";
 import type { PrismaClient } from "@calcom/prisma";
 import { BookingStatus, WebhookTriggerEvents } from "@calcom/prisma/enums";
-import { bookingMetadataSchema } from "@calcom/prisma/zod-utils";
 import type { AdditionalInformation, CalendarEvent } from "@calcom/types/Calendar";
 
 const log = logger.getChildLogger({ prefix: ["[handleConfirmation] book:user"] });
@@ -248,41 +245,6 @@ export async function handleConfirmation(args: {
       },
     });
     updatedBookings.push(updatedBooking);
-  }
-
-  const isKYCVerified = isEventTypeOwnerKYCVerified(updatedBookings[0].eventType);
-
-  //Workflows - set reminders for confirmed events
-  try {
-    for (let index = 0; index < updatedBookings.length; index++) {
-      if (updatedBookings[index].eventType?.workflows) {
-        const evtOfBooking = evt;
-        evtOfBooking.startTime = updatedBookings[index].startTime.toISOString();
-        evtOfBooking.endTime = updatedBookings[index].endTime.toISOString();
-        evtOfBooking.uid = updatedBookings[index].uid;
-        const eventTypeSlug = updatedBookings[index].eventType?.slug || "";
-
-        const isFirstBooking = index === 0;
-        const videoCallUrl =
-          bookingMetadataSchema.parse(updatedBookings[index].metadata || {})?.videoCallUrl || "";
-
-        await scheduleWorkflowReminders({
-          workflows: updatedBookings[index]?.eventType?.workflows || [],
-          smsReminderNumber: updatedBookings[index].smsReminderNumber,
-          calendarEvent: {
-            ...evtOfBooking,
-            ...{ metadata: { videoCallUrl }, eventType: { slug: eventTypeSlug } },
-          },
-          isFirstRecurringEvent: isFirstBooking,
-          hideBranding: !!updatedBookings[index].eventType?.owner?.hideBranding,
-          eventTypeRequiresConfirmation: true,
-          isKYCVerified,
-        });
-      }
-    }
-  } catch (error) {
-    // Silently fail
-    console.error(error);
   }
 
   try {
